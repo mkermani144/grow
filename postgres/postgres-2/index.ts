ad5371671113df75e26ca8e8cb07e25b8286e3f8
Mohammad Kermani <mkermani144@gmail.com>
2023-11-05 09:00:37 +0330

    postgres: get started with knex

---
+++ postgres/postgres-2/index.ts
@@ -0,0 +1,20 @@
+import knexFactory from "knex";
+
+const knex = knexFactory({
+  client: "pg",
+  connection: "postgresql://postgres:1234@localhost:5432",
+});
+
+await knex.schema.createTable("hello_world", (table) => {
+  table.integer("id").primary();
+  table.string("greeter");
+});
+
+await knex("hello_world").insert({ id: 1, greeter: "Asghar" });
+await knex("hello_world").insert({ id: 2, greeter: "Akbar" });
+
+console.log(await knex.select("*").from("hello_world"));
+
+await knex.schema.dropTable("hello_world");
+
+process.exit(0);
