a3415c35be9529479b15d5c290561da225a82fce
Mohammad Kermani <mkermani144@gmail.com>
2023-11-22 17:22:11 +0330

    postgres: add basic window functions script

---
+++ postgres/postgres-15/index.ts
@@ -0,0 +1,41 @@
+/**
+ * basic window functions
+ */
+import knexFactory from "knex";
+
+let knex = knexFactory({
+  client: "pg",
+  connection: "postgresql://postgres:1234@localhost:5432",
+});
+
+const TABLE_NAME = "users";
+
+await knex.schema.createTable(TABLE_NAME, (table) => {
+  table.text("username").primary();
+  table.text("name").notNullable();
+  table.enum("gender", ["male", "female"]);
+  table.integer("age").notNullable();
+  table.integer("revenue").defaultTo(0);
+  table.index("username");
+});
+
+await Promise.all([
+  knex(TABLE_NAME).insert({
+    username: "mkermani144",
+    name: "Mohammad Kermani",
+    age: 27,
+    revenue: 10000,
+  }),
+  knex(TABLE_NAME).insert({
+    username: "anonymous",
+    name: "Anonymous",
+    age: 33,
+    revenue: 1000000,
+  }),
+]);
+
+console.warn(await knex(TABLE_NAME).select(knex.raw("*, COUNT(*) OVER()")));
+
+await knex.schema.dropTableIfExists(TABLE_NAME);
+
+process.exit(0);
