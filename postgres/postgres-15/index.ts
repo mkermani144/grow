89f9843293ce7f32c6376722c4b67b901046167b
Mohammad Kermani <mkermani144@gmail.com>
2023-11-23 18:18:54 +0330

    postgres: update window functions script

--- postgres/postgres-15/index.ts
+++ postgres/postgres-15/index.ts
@@ -1,5 +1,5 @@
 /**
- * basic window functions
+ * window functions
  */
 import knexFactory from "knex";
 
@@ -23,18 +23,47 @@ await Promise.all([
   knex(TABLE_NAME).insert({
     username: "mkermani144",
     name: "Mohammad Kermani",
+    gender: "male",
     age: 27,
     revenue: 10000,
   }),
   knex(TABLE_NAME).insert({
     username: "anonymous",
     name: "Anonymous",
+    gender: "male",
     age: 33,
     revenue: 1000000,
   }),
+  knex(TABLE_NAME).insert({
+    username: "mrs. anonymous",
+    name: "MrsAnonymous",
+    gender: "female",
+    age: 22,
+    revenue: 200000,
+  }),
+  knex(TABLE_NAME).insert({
+    username: "mrs. unknown",
+    name: "MrsUnknown",
+    gender: "female",
+    age: 16,
+    revenue: 10000,
+  }),
+  knex(TABLE_NAME).insert({
+    username: "unknown",
+    name: "Unknown",
+    gender: "male",
+    age: 21,
+    revenue: 50000,
+  }),
 ]);
 
-console.warn(await knex(TABLE_NAME).select(knex.raw("*, COUNT(*) OVER()")));
+console.warn(
+  await knex(TABLE_NAME).select(
+    knex.raw(
+      "*, RANK() OVER(PARTITION BY gender ORDER BY revenue DESC) as revenue_rank_in_gender"
+    )
+  )
+);
 
 await knex.schema.dropTableIfExists(TABLE_NAME);
 
