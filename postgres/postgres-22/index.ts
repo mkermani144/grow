f65baf16ee9647a7042aaeb908ed7f9d9506f821
Mohammad Kermani <mkermani144@gmail.com>
2023-12-02 22:15:57 +0330

    postgres: add transaction savepoints script

---
+++ postgres/postgres-22/index.ts
@@ -0,0 +1,48 @@
+/**
+ * Transaction savepoints
+ */
+import knexFactory from "knex";
+
+let knex = knexFactory({
+  client: "pg",
+  connection: "postgresql://postgres:1234@localhost:5432",
+});
+
+const TABLE_NAME = "users";
+
+await knex.schema.createTable(TABLE_NAME, (table) => {
+  table.text("username").primary();
+  table.text("name").notNullable();
+  table.enum("gender", ["male", "female"]);
+  table.integer("age").notNullable();
+  table.integer("revenue").defaultTo(0);
+  table.index("username");
+});
+
+try {
+  await knex.transaction(async (trx) => {
+    await trx(TABLE_NAME).insert({
+      username: "mkermani144",
+      name: "Mohammad Kermani",
+      age: 27,
+      revenue: 10000,
+    });
+    await trx.raw("SAVEPOINT sp");
+    try {
+      await trx(TABLE_NAME).insert({
+        name: "Anonymous",
+        age: 33,
+        revenue: 500000,
+      });
+    } catch {
+      await trx.raw("ROLLBACK TO sp");
+    }
+  });
+} catch {
+} finally {
+  console.log(await knex(TABLE_NAME).select("*"));
+}
+
+await knex.schema.dropTableIfExists(TABLE_NAME);
+
+process.exit(0);
